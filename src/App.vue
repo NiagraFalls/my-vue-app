d96be0ecd7bf7a2d977d4aaa06950aad0a71b447
Jack Chan <jack.cheng@grapecity.com>
2024-10-16 14:34:08 +0800

    统一提交

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  <div>
+    <gc-spread-sheets
+      :hostClass='hostClass'
+    >
+        </gc-spread-sheets>
+    </div>
+  </template>
+  <script>
+  import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2016colorful.css'
+  import '@grapecity/spread-sheets-vue'
+  export default {
+  name:"App",
+  data(){
+    return {
+      hostClass:'spread-host',
+      autoGenerateColumns:true,
+      width:300,
+      visible:true,
+      resizable:true,
+      formatter:"$ #.00"
+    }
+  },
+  computed:{
+    dataTable(){
+      let dataTable = [];
+      for (let i = 0; i < 42; i++) {
+        dataTable.push({price: i + 0.56})
+      }
+      return dataTable
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  }
+  </script>
+  <style>
+  .spread-host {
+  width: 500px;
+  height: 600px;
+  }
+  </style>
+  
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
+import "@grapecity/spread-sheets-print";
+import "@grapecity/spread-sheets-shapes";
+import "@grapecity/spread-sheets-pivot-addon";
+import "@grapecity/spread-sheets-tablesheet";
+import { GcSpreadSheets } from "@grapecity/spread-sheets-vue"
 
-createApp(App).mount('#app')
+// component为注册应用范围内的组件，字符串，对象形式
+// mount将应用实例挂载到容器元素中，#app是vue给创建的，直接挂载即可
+createApp(App).component("GcSpreadSheets", GcSpreadSheets).mount('#app')
